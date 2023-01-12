5b26cbc9f190052fc724f86d0e863752465219c1
jordiascension <jordiascension@enterpriseconsulting.es>
2023-01-12 17:33:29 +0100

    Added PensionPlan Scaffolding

---
+++ CalculatorProject/PlanDePensiones/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.PlanDePensiones
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Up and Running");
+        }
+    }
+}
