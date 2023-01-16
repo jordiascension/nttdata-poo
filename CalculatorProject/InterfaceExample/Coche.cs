b52962d4c6a5bd78a094d539ce5adb857110c58f
jordiascension <jordiascension@enterpriseconsulting.es>
2023-01-16 15:44:14 +0100

    Added Polymophism Exercice with abstract classes and interfaces

---
+++ CalculatorProject/InterfaceExample/Coche.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProject.InterfaceExample
+{
+    internal class Coche : IVehiculo , ICloneable
+    {
+        public void Arrancar()
+        {
+            Console.WriteLine("Arrancar Coche");
+        }
+
+        public object Clone()
+        {
+            return new Coche();
+        }
+
+        public void Parar()
+        {
+            Console.WriteLine("Parar Coche");
+        }
+    }
+}
